3faee3ef1833f017d3c12c8cdafc5d66ae5de066
xtreme22886 <erik.w.clay@gmail.com>
2022-04-23 14:44:49 -0700

    Update unifi-presence-sensor.groovy

--- Driver/unifi-presence-sensor.groovy
+++ Driver/unifi-presence-sensor.groovy
@@ -1,1 +1,31 @@
+/**
+ *  UniFi Presence Sensor
+ *
+ *  Copyright 2019 xtreme
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
 
+metadata {
+	definition (name: "UniFi Presence Sensor", namespace: "xtreme22886", author: "xtreme", ocfDeviceType: "capability.presenceSensor") {
+		capability "PresenceSensor"
+		command "setPresence"
+	}
+}
+
+def setPresence(status) {
+	if (status == false) {
+		status = "not present"
+	} else {
+		status = "present"
+	}
+	sendEvent(name: "presence", value: status)
+}
